21d479f2c44fda27186590bb9539f1859bd2d384
abysstone <manasbhatt007@gmail.com>
2022-01-05 09:54:32 +0530

    serving through html docs

--- app.js
+++ app.js
@@ -9,17 +9,18 @@ const app = express();
 const adminRoutes = require('./routes/admin');
 
 const shopRoutes = require('./routes/shop');
-// const { send } = require('express/lib/response');
 
 app.use(bodyParser.urlencoded({extended: false}));
+
+const path = require('path');
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page not found!</h1>');
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
-
+ 
 // const server = http.createServer(app);
 // server.listen(3000);
 
